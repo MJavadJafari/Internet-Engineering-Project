8bfecc7caa1277a14a453f37d035239b2b5d8cbd
Javad <mjavad.jafari80@gmail.com>
2023-04-20 16:28:08 +0430

    git ignore + new req.txt

--- recommender/main.py
+++ recommender/main.py
@@ -1,11 +1,14 @@
-import numpy as np 
+import numpy as np
 from embedding import SentEmbedding
 from hazm import POSTagger
 from sklearn.metrics.pairwise import cosine_similarity
 import embedRank
-from flask import Flask, request
+from flask import Flask, request, make_response, jsonify
 from SequenceTagger import POSTagger
 
+app = Flask(__name__)
+sent2vec_path = r'C:\Users\delta\PycharmProjects\net\recommender\sent2vec-naab.model'
+posTagger_path = r'C:\Users\delta\PycharmProjects\net\recommender\posTagger.model'
 
 
 class SingletonRecommender:
@@ -15,7 +18,7 @@ class SingletonRecommender:
             cls.instance = super(SingletonRecommender, cls).__new__(cls)
         return cls.instance
 
-    def init_model(self, book_data, sent2vec_path = '/home/ebrahim/Desktop/sent2vec/sent2vec-naab.model', posTagger_path = '/home/ebrahim/training/postagger.model'):
+    def init_model(self, book_data, sent2vec_path=sent2vec_path, posTagger_path=posTagger_path):
         self.embedding_model = SentEmbedding(model_path= sent2vec_path)
         self.posTagger = POSTagger(model = posTagger_path)
         tmp_dic = {}
@@ -28,7 +31,8 @@ class SingletonRecommender:
     def insert_book(self, id: int, summary: str):
         keywords = np.unique(embedRank.embedRank(summary, max(4, len(summary.split()) / 8), self.embedding_model, self.posTagger))
         self.book_data[id] = [self.embedding_model[keyword] for keyword in keywords]
-        return keywords
+        # return keywords in list
+        return keywords.tolist()
 
     def delete_book(self, id):
         self.book_data.pop(id)
@@ -44,54 +48,44 @@ class SingletonRecommender:
         similar_indices = sorted(sim_dic, key=sim_dic.get, reverse=True)
 
         return similar_indices[1:]
-        # similarity_element = cosine_similarity(selected_vec, list(self.book_data.values()))
-        # similar_indices = similarity_element.argsort()
-        # similar_indices = np.flip(similar_indices)
-
-        # books = np.array(list(self.book_data))
-
-        # return list(books[similar_indices][0][1:])
 
 
-if __name__ == '__main__':
-
-    # recommender.init_model({})
-    # recommender.insert_book(2, 'دانشجویان این دانشگاه در حال بررسی پروژه خود هستند.')
-    # recommender.insert_book(5, 'دانش‌آموزان این مدرسه در تلاش برای انجام تمارین هستند.')
-    # recommender.insert_book(6, 'در جنگل ایران ببرهای بسیاری وجود دارد که در معرض خطر هستند.')
-    # recommender.insert_book(7, 'تاریخ بیهقی شامل فراز و نشیب بسیار است.')
-    # print(recommender.ask_book(2))
-    # recommender.print_books()
-    
-    app = Flask(__name__)
-    recommender = SingletonRecommender()
-    recommender.init_model({})
-    app.run(port = 5000)
-
-@app.route('/init_model')
+@app.route('/init_model', methods=['POST'])
 def init_model():
-    book_data = request.args.get('book_data')
-    sent2vec_path = request.args.get('sent2vec_path')
-    posTagger_path = request.args.get('posTagger_path')
+    book_data = request.form.get('book_data')
     recommender.init_model(book_data, sent2vec_path, posTagger_path)
+    return 'success'
 
-@app.route('/insert_book')
+
+@app.route('/insert_book', methods=['POST'])
 def insert_book():
-    id = request.args.get('id')
-    summary = request.args.get('summary')
-    return recommender.insert_book(id, summary)
+    id = int(request.form.get('id'))
+    summary = request.form.get('summary')
+    result = recommender.insert_book(id, summary)
+    print(result)
+    return jsonify(result)
 
-@app.route('/delete_book')
+
+@app.route('/delete_book', methods=['POST'])
 def delete_book():
-    id = request.args.get('id')
+    id = int(request.form.get('id'))
     recommender.delete_book(id)
+    return 'success'
+
 
 @app.route('/print_books')
 def print_books():
     recommender.print_books()
+    return 'success'
 
-@app.route('/ask_book')
+
+@app.route('/ask_book', methods=['POST'])
 def ask_book():
-    id = request.args.get('id')
+    id = int(request.form.get('id'))
     return recommender.ask_book(id)
 
+
+if __name__ == '__main__':
+    recommender = SingletonRecommender()
+    recommender.init_model({})
+    app.run(port=5000)
