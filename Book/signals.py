f86c20ce67c9340e23e21971597ff32e3eed5b85
SaraKehtari <sara80.kehtari@gmail.com>
2023-04-20 14:18:59 +0330

    feat: add book_created signal

--- Book/signals.py
+++ Book/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
-from Book.models import BookRequest
+from Book.models import BookRequest, Book
 
 
 @receiver(post_save, sender=BookRequest)
@@ -17,3 +17,11 @@ def book_request_deleted(sender, instance, **kwargs):
     book = instance.book
     book.number_of_request = max(book.number_of_request - 1, 0)
     book.save()
+
+@receiver(post_save, sender=Book)
+def book_created(sender, instance, created, **kwargs):
+    if created:
+        book = instance.book
+        # TODO: add keywords api
+        book.keywords = null
+        book.save()
