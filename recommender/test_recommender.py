cffba3c6e244b63721de009cbfc296c1c18b7a7e
E_Ghafour <e.ghafour2000@gmail.com>
2023-05-24 16:21:18 +0330

    fix some problems for in recommender tests

--- recommender/test_recommender.py
+++ recommender/test_recommender.py
@@ -7,7 +7,7 @@ from embedding import *
 
 #prepared data
 embedding_path = '/home/ebrahim/training/sent2vecModel/sent2vec-test.model'
-tagger_path = '/home/ebrahim/repos/Kahroba/Internet-Engineering-Project/python-crf_uni_iter400_train95_rs10_c1-0.4_c2-0.04.model'
+tagger_path = '/home/ebrahim/repos/Kahroba/Internet-Engineering-Project/pos_tagger.model'
 big_sample_text = 'سفارت ایران در مادرید درباره فیلم منتشرشده از «حسن قشقاوی» در مراسم سال نو در کاخ سلطنتی اسپانیا و حاشیه‌سازی‌ها در فضای مجازی اعلام کرد: به تشریفات دربار کتباً اعلام شد سفیر بدون همراه در مراسم حضور خواهد داشت و همچون قبل به دلایل تشریفاتی نمی‌تواند با ملکه دست بدهد. همان‌گونه که کارشناس رسمی تشریفات در توضیحات خود به یک نشریه اسپانیایی گفت این موضوع توضیح مذهبی داشته و هرگز به معنی بی‌احترامی به مقام و شخصیت زن آن هم در سطح ملکه محترمه یک کشور نیست.'
 small_sample_text = 'در جنگل ایران گونه‌های جانوری زیادی وجود دارد.'
 small_sample_text1 = 'آمازون شامل ببرهای وحشی زیادی است.'
