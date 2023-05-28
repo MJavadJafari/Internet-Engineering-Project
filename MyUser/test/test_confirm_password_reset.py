b02cf3ac835a33e8106c851ac342496397f5f9d1
SaraKehtari <sara80.kehtari@gmail.com>
2023-05-28 11:24:31 +0330

    test: ConfirmPasswordReset api in MyUser

---
+++ MyUser/test/test_confirm_password_reset.py
@@ -0,0 +1,61 @@
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth import get_user_model
+from MyUser.models import EmailToken
+import random
+import string
+
+class ConfirmPasswordResetTests(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # create user
+        cls.user = get_user_model().objects.create_user(
+            email='test@example.com',
+            password='testpassword',
+            name='abc',
+            phone_number='123456789'
+        )
+        cls.token = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(30))
+        cls.email_token = EmailToken.objects.create(user=cls.user, token=cls.token)
+        cls.url = '/auth/confirm_password_reset/'
+
+    def test_confirm_password_reset_should_render_form(self):
+        # Arrange
+
+        # Act
+        response = self.client.get((f'{self.url}{self.token}'))
+
+        # Assert
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTemplateUsed(response, 'password_reset_success.html')
+
+    def test_confirm_password_reset_should_return_400_when_token_is_not_found(self):
+        # Arrange
+        token = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(30))
+
+        # Act
+        response = self.client.get((f'{self.url}{token}'))
+
+        # Assert
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {'Invalid user'})
+    
+    def test_confirm_password_reset_should_return_400_when_token_is_empty(self):
+        # Arrange
+        token = ''
+
+        # Act
+        response = self.client.get((f'{self.url}{token}'))
+
+        # Assert
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_confirm_password_reset_should_change_old_password(self):
+        # Arrange
+
+        # Act
+        response = self.client.get((f'{self.url}{self.token}'))
+
+        # Assert
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertFalse(self.user.check_password(self.user.password))
